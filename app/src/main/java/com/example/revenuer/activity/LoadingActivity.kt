c73b2dd318f4753b6f4aca8d23ae47791446d7da
Samuel Zaire <samuel.zairerocha@gmail.com>
2022-05-30 19:23:21 -0300

    added layouts classes and xml

---
+++ app/src/main/java/com/example/revenuer/activity/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.revenuer.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.revenuer.R
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
