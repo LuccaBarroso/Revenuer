f1a56b1cfb16e72a11f0b81109f37198707e90b5
Samuel Zaire <samuel.zairerocha@gmail.com>
2022-06-01 19:20:25 -0300

    revenuer v1.1 - edited manifest, implemented activities and loading and login screens design

--- app/src/main/java/com/example/revenuer/activity/LoadingActivity.kt
+++ app/src/main/java/com/example/revenuer/activity/LoadingActivity.kt
@@ -1,12 +1,26 @@
 package com.example.revenuer.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.example.revenuer.R
 
 class LoadingActivity : AppCompatActivity() {
+//    private val actionBar: ActionBar? = supportActionBar
     override fun onCreate(savedInstanceState: Bundle?) {
+//        actionBar?.hide()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loading)
+
+        val handler = Handler(Looper.myLooper()!!)
+
+        handler.postDelayed({
+            val it = Intent(LoadingActivity@this, LoginActivity::class.java);
+            startActivity(it);
+            finish();
+        }, 3000L)
     }
 }
--- app/src/main/java/com/example/revenuer/activity/LoginActivity.kt
+++ app/src/main/java/com/example/revenuer/activity/LoginActivity.kt
@@ -1,12 +1,70 @@
 package com.example.revenuer.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.revenuer.R
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mLoginEmail: EditText;
+    private lateinit var mLoginPassword: EditText;
+    private lateinit var mLoginRegister: TextView;
+    private lateinit var mLoginEnter: Button;
+
+    private val handler =Handler(Looper.getMainLooper()!!)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        mLoginEmail = findViewById(R.id.login_edittext_email)
+        mLoginPassword = findViewById(R.id.login_edittext_password)
+        mLoginRegister = findViewById(R.id.login_textview_register)
+        mLoginRegister.setOnClickListener(this);
+        mLoginEnter = findViewById(R.id.login_button_enter)
+        mLoginEnter.setOnClickListener(this);
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.login_textview_register ->{
+                val it = Intent(this, RegisterActivity::class.java);
+                startActivity(it)
+            }
+
+            R.id.login_button_enter-> {
+                val email = mLoginEmail.text.toString()
+                val password = mLoginPassword.text.toString()
+
+                var isFormFilled = true
+
+                isFormFilled  =  isEmailFilled(email) && isFormFilled;
+                isFormFilled  =  isPasswordFilled(password) && isFormFilled;
+            }
+        }
+    }
+    private fun isPasswordFilled(password: CharSequence): Boolean {
+        return if (password.isBlank()) {
+            mLoginPassword.error = "Este campo é obrigatório"
+            false
+        }else {
+            true
+        }
+    }
+
+    private fun isEmailFilled(email: CharSequence): Boolean {
+        return if (email.isBlank()) {
+            mLoginEmail.error = "Este campo é obrigatório"
+            false
+        }else {
+            true
+        }
     }
 }
